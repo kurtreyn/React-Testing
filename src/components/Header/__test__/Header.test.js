82c291139c77191cc8da498fe045819a94b58ae2
KurtReyn <kurtreyn@yahoo.com>
2022-07-27 12:02:19 -0400

    continue with practicing unit testing

--- src/components/Header/__test__/Header.test.js
+++ src/components/Header/__test__/Header.test.js
@@ -1,90 +1,23 @@
 import { render, screen } from '@testing-library/react';
 import Header from '../Header';
 
-describe("Header", () => {
-    it('should render same text passed into title prop', () => {
-        render(
-            <Header 
-              title="todo"
-            />
-        );
-        const h1Element = screen.getByText(/todo/i);
-        expect(h1Element).toBeInTheDocument();
-    });
-})
-
-// it('should render same text passed into title prop', () => {
-//     render(
-//         <Header 
-//           title="todo"
-//         />
-//     );
-//     const h1Element = screen.getByRole("heading");
-//     expect(h1Element).toBeInTheDocument();
+// getByText
+// test('should render same text passed into title prop', async () => {
+//   render(<Header title="My header" />);
+//   const headingElement = screen.getByText(/my header/i);
+//   expect(headingElement).toBeInTheDocument();
 // });
 
-// it('should render same text passed into title prop', () => {
-//     render(
-//         <Header 
-//           title="todo"
-//         />
-//     );
-//     const h1Element = screen.getByRole("heading", { name: /todo/i });
-//     expect(h1Element).toBeInTheDocument();
+// getByRole (if the role was switched to something other than heading, it would fail the test) This also fails if there are multiple headers.
+// it('this should render same text passed into title prop', async () => {
+//   render(<Header title="My header" />);
+//   const headingElement = screen.getByRole('heading');
+//   expect(headingElement).toBeInTheDocument();
 // });
 
-// it('should render same text passed into title prop', () => {
-//     render(
-//         <Header 
-//           title="todo"
-//         />
-//     );
-//     const h1Element = screen.getByTitle("Header");
-//     expect(h1Element).toBeInTheDocument();
-// });
-
-// it('should render same text passed into title prop', () => {
-//     render(
-//         <Header 
-//           title="todo"
-//         />
-//     );
-//     const h2Element = screen.getByTestId("header-2");
-//     expect(h2Element).toBeInTheDocument();
-// });
-
-// // WITH FINDBY
-
-// it('should render same text passed into title prop', async () => {
-//     render(
-//         <Header 
-//           title="todo"
-//         />
-//     );
-//     const h1Element = await screen.findByText(/todo/i);
-//     expect(h1Element).toBeInTheDocument();
-// });
-
-// // WITH QUERYBY
-
-// it('should render same text passed into title prop', () => {
-//     render(
-//         <Header 
-//           title="todo"
-//         />
-//     );
-//     const h1Element = screen.queryByText(/dogs/i);
-//     expect(h1Element).not.toBeInTheDocument
-// });
-
-// // WITH GETALLBY
-
-// it('should render same text passed into title prop', () => {
-//     render(
-//         <Header 
-//           title="todo"
-//         />
-//     );
-//     const h1Elements = screen.getAllByText(/todo/i);
-//     expect(h1Elements.length).toBe(1);
-// });
+// when there is a second header, in this case, the second header is an h3 that holds the text "Cats"
+it('expected to render same text passed into title prop', async () => {
+  render(<Header title="My Header" />);
+  const headingElement = screen.getByRole('heading', { name: 'My Header' });
+  expect(headingElement).toBeInTheDocument();
+});
