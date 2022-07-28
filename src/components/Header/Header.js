dd65effd33f3e9ee6c6e6e94a2c95bbc9ad91002
KurtReyn <kurtreyn@yahoo.com>
2022-07-27 20:15:49 -0400

    query methods

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,8 +4,12 @@ import './Header.css';
 export default function Header({ title }) {
   return (
     <>
-      <h1 className="header">{title}</h1>
-      <h3 className="header">Cats</h3>
+      <h1 className="header" data-testid="header-1">
+        {title}
+      </h1>
+      <h3 title="Header" className="header">
+        Cats
+      </h3>
     </>
   );
 }
--- src/components/Header/__test__/Header.test.js
+++ src/components/Header/__test__/Header.test.js
@@ -2,11 +2,11 @@ import { render, screen } from '@testing-library/react';
 import Header from '../Header';
 
 // getByText
-// test('should render same text passed into title prop', async () => {
-//   render(<Header title="My header" />);
-//   const headingElement = screen.getByText(/my header/i);
-//   expect(headingElement).toBeInTheDocument();
-// });
+test('should render same text passed into title prop', async () => {
+  render(<Header title="My header" />);
+  const headingElement = screen.getByText(/my header/i);
+  expect(headingElement).toBeInTheDocument();
+});
 
 // getByRole (if the role was switched to something other than heading, it would fail the test) This also fails if there are multiple headers.
 // it('this should render same text passed into title prop', async () => {
@@ -20,4 +20,39 @@ it('expected to render same text passed into title prop', async () => {
   render(<Header title="My Header" />);
   const headingElement = screen.getByRole('heading', { name: 'My Header' });
   expect(headingElement).toBeInTheDocument();
+});
+
+// this should test the h3 element which has a title of "Header"
+it('expect to render same text passed into title prop', async () => {
+  render(<Header title="My Header" />);
+  const headingElement = screen.getByTitle('Header');
+  expect(headingElement).toBeInTheDocument();
+});
+
+// this should test the h1 element with data-testid of "header-1"
+it('this should render same text passed into title prop', async () => {
+  render(<Header title="My Header" />);
+  const headingElement = screen.getByTestId('header-1');
+  expect(headingElement).toBeInTheDocument();
+});
+
+// findByText (use findBy when it needs to be async)
+test('test should render same text passed into title prop', async () => {
+  render(<Header title="My header" />);
+  const headingElement = await screen.findByText(/my header/i);
+  expect(headingElement).toBeInTheDocument();
+});
+
+// queryBy use when you want to expect something to not be there
+test('this test should render same text passed into title prop', async () => {
+  render(<Header title="My header" />);
+  const headingElement = screen.queryByText(/dogs/i);
+  expect(headingElement).not.toBeInTheDocument();
+});
+
+// getAllByRole (use getAllBy when you need to test multiple elements)
+test('this test should render same text passed into title prop of element', async () => {
+  render(<Header title="My header" />);
+  const headingElements = screen.getAllByRole('heading');
+  expect(headingElements.length).toBe(2);
 });
