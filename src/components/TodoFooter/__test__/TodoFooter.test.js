ec46f2208124558479d97db11aca047503465916
KurtReyn <kurtreyn@yahoo.com>
2022-07-28 02:19:29 -0400

    describe block

--- src/components/TodoFooter/__test__/TodoFooter.test.js
+++ src/components/TodoFooter/__test__/TodoFooter.test.js
@@ -12,42 +12,45 @@ const MockTodoFooter = ({ numberOfIncompleteTasks }) => {
   );
 };
 
-test('should render the correct amount of incomplete tasks', async () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={5} />);
-  const paragraphElement = screen.getByText(/5 tasks left/i);
-  expect(paragraphElement).toBeInTheDocument();
+// You can have nested describe blocks
+describe('TodoFooter', () => {
+  test('should render the correct amount of incomplete tasks', async () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={5} />);
+    const paragraphElement = screen.getByText(/5 tasks left/i);
+    expect(paragraphElement).toBeInTheDocument();
+  });
+
+  test('should render "task" when the number of incomplete tasks in one', async () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+    const paragraphElement = screen.getByText(/1 task left/i);
+    expect(paragraphElement).toBeInTheDocument();
+  });
 });
 
-test('should render "task" when the number of incomplete tasks in one', async () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-  const paragraphElement = screen.getByText(/1 task left/i);
-  expect(paragraphElement).toBeInTheDocument();
-});
-
-test('TRUE of FALSE: should render "task" when the number of incomplete tasks in one', async () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-  const paragraphElement = screen.getByText(/1 task left/i);
-  expect(paragraphElement).toBeTruthy();
-});
+// test('TRUE of FALSE: should render "task" when the number of incomplete tasks in one', async () => {
+//   render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+//   const paragraphElement = screen.getByText(/1 task left/i);
+//   expect(paragraphElement).toBeTruthy();
+// });
 
 // if the opacity is 0, it will not be in the document and the test fails
-test('IS IT VISIBLE TO USER?', async () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-  const paragraphElement = screen.getByText(/1 task left/i);
-  expect(paragraphElement).toBeVisible();
-});
+// test('IS IT VISIBLE TO USER?', async () => {
+//   render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+//   const paragraphElement = screen.getByText(/1 task left/i);
+//   expect(paragraphElement).toBeVisible();
+// });
 
-test('see if the element contains a specific element', async () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-  const paragraphElement = screen.getByText(/1 task left/i);
-  expect(paragraphElement).toContainHTML('p');
-});
+// test('see if the element contains a specific element', async () => {
+//   render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+//   const paragraphElement = screen.getByText(/1 task left/i);
+//   expect(paragraphElement).toContainHTML('p');
+// });
 
-test('see element has specified content', async () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-  const paragraphElement = screen.getByTestId('para');
-  expect(paragraphElement).toHaveTextContent('1 task left');
-});
+// test('see element has specified content', async () => {
+//   render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+//   const paragraphElement = screen.getByTestId('para');
+//   expect(paragraphElement).toHaveTextContent('1 task left');
+// });
 
 // test will only pass if the element is not visible
 // test('see element does not contain specified content', async () => {
@@ -56,15 +59,15 @@ test('see element has specified content', async () => {
 //   expect(paragraphElement).not.toBeVisible();
 // });
 
-test('see element is not falsey', async () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-  const paragraphElement = screen.getByTestId('para');
-  expect(paragraphElement).not.toBeFalsy();
-});
+// test('see element is not falsey', async () => {
+//   render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+//   const paragraphElement = screen.getByTestId('para');
+//   expect(paragraphElement).not.toBeFalsy();
+// });
 
-test('see element returns given text content', async () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-  const paragraphElement = screen.getByTestId('para');
-  expect(paragraphElement.textContent).toBe('1 task left');
-  // multiple assertions can be made, but not recommended
-});
+// test('see element returns given text content', async () => {
+//   render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+//   const paragraphElement = screen.getByTestId('para');
+//   expect(paragraphElement.textContent).toBe('1 task left');
+//   // multiple assertions can be made, but not recommended
+// });
